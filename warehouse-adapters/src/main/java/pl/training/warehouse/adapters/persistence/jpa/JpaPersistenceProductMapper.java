d25b5146a07bd89ab0f49f9119dd1ee1613df7b9
Łukasz Andrzejewski <landrzejewski.poczta@gmail.com>
2022-09-06 10:24:57 +0200

    warehouse

--- warehouse-adapters/src/main/java/pl/training/warehouse/adapters/persistence/jpa/JpaPersistenceProductMapper.java
+++ warehouse-adapters/src/main/java/pl/training/warehouse/adapters/persistence/jpa/JpaPersistenceProductMapper.java
@@ -2,12 +2,9 @@ package pl.training.warehouse.adapters.persistence.jpa;
 
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
-import pl.training.commons.money.Currency;
 import pl.training.warehouse.ports.Product;
 
-import java.math.BigDecimal;
-
-@Mapper(componentModel = "spring", imports = {BigDecimal.class, Currency.class})
+@Mapper(componentModel = "spring")
 public interface JpaPersistenceProductMapper {
 
     @Mapping(source = "price", target = "value")
--- warehouse-domain/src/main/java/pl/training/warehouse/domain/ProductDomain.java
+++ warehouse-domain/src/main/java/pl/training/warehouse/domain/ProductDomain.java
@@ -1,13 +1,12 @@
 package pl.training.warehouse.domain;
 
 import lombok.Value;
-import org.javamoney.moneta.FastMoney;
 
 @Value
 class ProductDomain {
 
     Long id;
     String name;
-    FastMoney price;
+    Double price;
 
 }
