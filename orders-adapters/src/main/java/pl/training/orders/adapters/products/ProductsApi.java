b5da5db9c5bc475b4bb49296961ed8266d185cea
Łukasz Andrzejewski <landrzejewski.poczta@gmail.com>
2022-09-06 12:21:32 +0200

    warehouse

--- orders-adapters/src/main/java/pl/training/orders/adapters/products/ProductsApi.java
+++ orders-adapters/src/main/java/pl/training/orders/adapters/products/ProductsApi.java
@@ -4,7 +4,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(value = "WAREHOUSE-SERVICE", name = "warehouse")
+@FeignClient("WAREHOUSE-SERVICE")
 public interface ProductsApi {
 
     @GetMapping("products/{id}")
