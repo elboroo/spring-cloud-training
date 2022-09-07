3c89071ea14c262d6513c259b742d5df5ab38c86
landrzejewski <landrzejewski.poczta@gmail.com>
2022-09-07 14:59:26 +0200

    z

---
+++ broker-service/src/main/java/pl/training/broker/BrokerService.java
@@ -0,0 +1,13 @@
+package pl.training.broker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrokerService {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BrokerService.class, args);
+    }
+
+}
