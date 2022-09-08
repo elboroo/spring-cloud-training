116061a28d2e11c9d3b57b476d21391c6f165301
landrzejewski <landrzejewski.poczta@gmail.com>
2022-09-08 14:38:54 +0200

    configwww

--- build.sh
+++ build.sh
@@ -5,5 +5,5 @@ docker build -t training/discovery-server discovery-server
 docker build -t training/gateway-server gateway-server
 docker build -t training/broker-service broker-service
 docker build -t training/warehouse-service warehouse-service
-#docker build -t training/payments-service payments-service
+docker build -t training/payments-service payments-service
 #docker build -t training/shop-service shop-service
